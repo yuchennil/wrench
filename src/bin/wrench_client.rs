7a18f4efd929d750b3b405cdddd0b42b34bdecf0
Yuchen Lin <yuchennil@gmail.com>
2020-08-24 09:43:24 -0700

    Modify wrench_client to async scan input + server

--- src/bin/wrench_client.rs
+++ src/bin/wrench_client.rs
@@ -1,58 +1,94 @@
 use std::io::{prelude::*, stdin};
 use std::net::{Shutdown, TcpStream};
+use std::sync::mpsc;
+use std::{thread, time};
 
 use wrench::{
     Client, Plaintext,
     Request::{self, *},
-    Response, UserId,
+    Response::{self, *},
+    UserId,
 };
 
 const SERVER_URL: &str = "127.0.0.1:39656";
 
+enum Packet {
+    Response(Response),
+    UserInput(String),
+}
+
 fn main() -> std::io::Result<()> {
     let mut client = Client::new().unwrap();
-    call_server(AddPrekeys(client.id(), client.publish_prekeys()))?;
     print_usage(&client)?;
+    let (tx, rx) = mpsc::channel();
+
+    spawn_server_call(&tx, AddPrekeys(client.id(), client.publish_prekeys()));
+    spawn_read_line_loop(&tx);
+    spawn_get_mail_loop(&tx, client.id());
 
     let mut peer_id = None;
-    loop {
-        let mail = call_server(GetMail(client.id()))?.mail().unwrap();
-        for envelope in mail {
-            let (user_id, plaintext) = client.receive(envelope).unwrap();
-            peer_id = Some(user_id.clone());
-            print!(
-                "{}: {}",
-                serde_json::to_string(&client.id())?,
-                std::str::from_utf8(&plaintext.0).unwrap(),
-            );
-        }
-        let line = read_line()?;
-        match (line.chars().next(), &peer_id) {
-            (Some('/'), _) => {
-                let user_id: UserId = serde_json::from_str(&line[1..])?;
-                peer_id = Some(user_id.clone());
-                if !client.has_session(&user_id) {
-                    let prekey = call_server(GetPrekey(user_id))?.prekey().unwrap();
-                    client.initiate(prekey).unwrap();
+    while let Ok(packet) = rx.recv() {
+        use Packet::*;
+        match packet {
+            Response(Success) => (),
+            Response(Error(error)) => panic!(error),
+            Response(Prekey(prekey)) => client.initiate(prekey).unwrap(),
+            UserInput(line) => match (line.chars().next(), &peer_id) {
+                (Some('/'), _) => {
+                    let user_id: UserId = serde_json::from_str(&line[1..])?;
+                    peer_id = Some(user_id.clone());
+                    if !client.has_session(&user_id) {
+                        spawn_server_call(&tx, GetPrekey(user_id));
+                    }
                 }
-            }
-            (_, Some(user_id)) => {
-                let plaintext = Plaintext(line.as_bytes().to_vec());
-                call_server(AddMail(client.send(user_id.clone(), plaintext).unwrap()))?;
-            }
-            (_, None) => {
-                println!("  error: no user_id specified");
-                print_usage(&client)?;
-                continue;
+                (_, Some(user_id)) => {
+                    let plaintext = Plaintext(line.as_bytes().to_vec());
+                    spawn_server_call(
+                        &tx,
+                        AddMail(client.send(user_id.clone(), plaintext).unwrap()),
+                    );
+                }
+                (_, None) => {
+                    println!("  error: no user_id specified");
+                    print_usage(&client)?;
+                }
+            },
+            Response(Mail(bundle)) => {
+                for envelope in bundle {
+                    let (user_id, plaintext) = client.receive(envelope).unwrap();
+                    peer_id = Some(user_id.clone());
+                    print!(
+                        "{}: {}",
+                        serde_json::to_string(&client.id())?,
+                        std::str::from_utf8(&plaintext.0).unwrap(),
+                    );
+                }
             }
         }
     }
+    Ok(())
 }
 
-fn read_line() -> std::io::Result<String> {
-    let mut buffer = String::new();
-    stdin().read_line(&mut buffer)?;
-    Ok(buffer)
+fn spawn_read_line_loop(tx: &mpsc::Sender<Packet>) {
+    let tx = tx.clone();
+    thread::spawn(move || loop {
+        let mut buffer = String::new();
+        stdin().read_line(&mut buffer).unwrap();
+        tx.send(Packet::UserInput(buffer)).unwrap();
+    });
+}
+
+fn spawn_get_mail_loop(tx: &mpsc::Sender<Packet>, user_id: UserId) {
+    let tx = tx.clone();
+    thread::spawn(move || loop {
+        thread::sleep(time::Duration::from_millis(1000));
+        spawn_server_call(&tx, GetMail(user_id.clone()));
+    });
+}
+
+fn spawn_server_call(tx: &mpsc::Sender<Packet>, request: Request) {
+    let tx = tx.clone();
+    thread::spawn(move || tx.send(Packet::Response(call_server(request).unwrap())));
 }
 
 fn call_server(request: Request) -> std::io::Result<Response> {
